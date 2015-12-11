814dbf188717f961bad3c44269023e71d75f6407
Anglerfish <keith.ching@gmail.com>
2015-12-11 15:55:15 -0800

    Added SMT-Program Schedule-edit.js
    Added to SMT-Program Schedule-edit.js to GitHub

---
+++ SMT-Program Schedule-edit.js
@@ -0,0 +1,13 @@
+﻿function commentBox() {
+$(document).ready(function(){
+  $("head").append('<link href="http://server1:8086/javascripts/jquery-ui-1104/css/redmond/jquery-ui-1.10.4.custom.min.css" rel="stylesheet" type="text/css" />' +
+                   '<link href="http://server1:8086/javascripts/SMT-Program Schedule-edit.css" rel="stylesheet" type="text/css" />' );
+  $("TABLE.ms-siteaction").hide();
+  
+  $("input[id$='DateTimeFieldDate']").datepicker({inline:true});
+//ckeditor for chrome/Firefox users
+  if(!browseris.ie5up) {
+    $("textarea[id$='TextField']").attr("class","ckeditor");
+  }
+});
+}
